00a0b47cc47afff06f231f5cb6f4ae698bb53c81
spox <82903@student.pb.edu.pl>
2024-04-22 20:06:34 +0200

    view posts list

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    posts = Post.objects.all().order_by('datePosted')
+    return render(request, 'blog/home.html', {'posts': posts})
