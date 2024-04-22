e3cd1de4dc2652a05a016c40b27316881d19d383
Krzysztof Pawlowski <krzysztofpeiv@o2.pl>
2024-04-22 20:11:03 +0200

    added register form

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home')
+    path('', views.home, name='blog-home'),
+    path('register', views.register, name='blog-register')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import CustomUser
+from django.contrib.auth.hashers import make_password
+
 # Create your views here.
 def home(request):
     return render(request, 'blog/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        hashed_password = make_password(password)  # Hash the password
+
+        # Create and save the user
+        user = CustomUser.objects.create(username=username, email=email, password=hashed_password)
+        user.save()
+        
+        # Redirect to a success page or any other page
+        return redirect('blog-home')
+    else:
+        return render(request, 'blog/register.html')
