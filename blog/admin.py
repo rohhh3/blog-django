c197872b6677379088e518e212bfb65e357ae92d
Krzysztof Pawlowski <krzysztofpeiv@o2.pl>
2024-05-24 16:46:08 +0200

    modified admin area for posts

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import Post, Comment, Category, CustomUser
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Category)
 admin.site.register(CustomUser)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'datePosted' , 'isPublic') #dodatkowe kolumny do post
+    list_filter = ('datePosted', 'isPublic')
+    ordering = ('title',)
+    search_fields = ('title', 'author', 'datePosted' , 'isPublic', 'categories')
+    fields = (('title', 'author', 'isPublic'), 'content', 'categories', 'thumbnail', 'datePosted') #struktura formularza tworzenia/edytowania 
+
