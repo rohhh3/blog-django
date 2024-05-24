ba9916b0bcb4a23de9a45e2bf22d42a1df8123f6
Krzysztof Pawlowski <krzysztofpeiv@o2.pl>
2024-05-24 17:31:28 +0200

    modified admin area for categories and fixed search bug for post

--- blog/admin.py
+++ blog/admin.py
@@ -3,14 +3,19 @@ from .models import Post, Comment, Category, CustomUser
 
 # Register your models here.
 admin.site.register(Comment)
-admin.site.register(Category)
 admin.site.register(CustomUser)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+    search_fields = ('name',)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'datePosted' , 'isPublic') #dodatkowe kolumny do post
-    list_filter = ('datePosted', 'isPublic')
+    list_filter = ('datePosted', 'isPublic') #filtrowanie
     ordering = ('title',)
-    search_fields = ('title', 'author', 'datePosted' , 'isPublic', 'categories')
+    search_fields = ('title', 'author__username')
     fields = (('title', 'author', 'isPublic'), 'content', 'categories', 'thumbnail', 'datePosted') #struktura formularza tworzenia/edytowania 
 
